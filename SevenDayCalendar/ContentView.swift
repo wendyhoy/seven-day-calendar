6638e857d9ecf2e9f8414444b42e5bee69a26a3c
Wendy Hoy <wendy.hoy@gmail.com>
2024-04-18 16:39:31 -0700

    Added a ScrollView with Text for each day of the week

--- SevenDayCalendar/ContentView.swift
+++ SevenDayCalendar/ContentView.swift
@@ -8,14 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    let daysOfTheWeek: [String] = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday"
+    ]
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Cute Animal Seven-Day Calendar")
+                .font(.headline)
+                .padding()
+            
+            ScrollView {
+                LazyVStack {
+                    ForEach(daysOfTheWeek, id: \.description) {
+                        day in Text(day)
+                            .font(.largeTitle)
+                            .padding(80)
+                            .frame(maxWidth: .infinity)
+                            .background(Rectangle()
+                                .fill(.red))
+                            .border(.black)
+                    }
+                }
+            }
         }
-        .padding()
+        .padding(10)
     }
 }
 
